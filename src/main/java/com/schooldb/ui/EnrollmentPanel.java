29f9813f5ff6d8ee046d769e176f1301b5bb6b7a
Laser <Arjietomaquin807@gmail.com>
2026-04-27 00:21:34 +0800

    Implement enroll form,buttons and logic

--- src/main/java/com/schooldb/ui/EnrollmentPanel.java
+++ src/main/java/com/schooldb/ui/EnrollmentPanel.java
@@ -25,14 +25,14 @@ public class EnrollmentPanel extends JPanel {
     private int selectedEnlistmentId = -1;
 
     //Enrollment Form
-    JComboBox<String> comboStudent = new JComboBox<>();
-    JComboBox<String> comboProgram = new JComboBox<>();
+    JComboBox comboStudent = new JComboBox<>();
+    JComboBox comboProgram = new JComboBox<>();
     JTextField fieldSchoolYear = new JTextField();
-    JComboBox<String> comboTerm = new JComboBox<>();
+    JComboBox comboTerm = new JComboBox<>();
 
     //Enlistment Form
     JTextField fieldEnrollId = new JTextField();
-    JComboBox<String> comboSection = new JComboBox<>();
+    JComboBox comboSection = new JComboBox<>();
     JTextField fieldGrade = new JTextField();
 
     //Buttons Enrollment
@@ -48,11 +48,224 @@ public class EnrollmentPanel extends JPanel {
     public EnrollmentPanel(DatabaseManager  dbManager){
         this.dbManager = dbManager;
 
-        //Initialize Table Models
-        enrollModel = new DefaultTableModel();
+        //Enrollment Table Model
+        enrollModel = new DefaultTableModel(
+            new String[]{
+                "Enrollment ID","Last Name","First Name","Program",
+                "School Year","Term","Date Enrolled"
+            }, 0
+        ){
+            public boolean isCellEditable(int row, int column){
+                return false;
+            }
+        };
         enrollTable = new JTable(enrollModel);
 
-        enlistModel = new DefaultTableModel();
+        //Enlistment Table Model
+        enlistModel = new DefaultTableModel(
+            new String[]{
+                "Enlistment ID","Section","Course Code","Title","Instructor",
+                "Days","Time","Room","Date Enlisted","Grade"
+        }, 0
+        ){
+        public boolean isCellEditable(int row, int column) {
+            return false;
+            }
+        };
         enlistTable = new JTable(enlistModel);
+
+        setLayout(null);
+        setBackground(Color.decode("#1a1a1a"));
+
+        //Populate Combos
+        try {
+            for (Object[] r : dbManager.getAllStudents()){
+                comboStudent.addItem(
+                    r[0] + " - " + r[1] + ", " + r[2]
+                );
+            }
+
+            for (Object[] r : dbManager.getAllPrograms()){
+                comboProgram.addItem(r[0].toString());
+            }
+
+            for (Objec[] r : dbManager.getAllSections()){
+                comboSection.addItem(r[0].toString());
+            }
+        } catch (SQLException ex){
+            showError("Failed to load data: " + ex.getMessage());
+        }
+
+        comboTerm.addItem("1st Sem");
+        comboTerm.addItem("2nd Sem");
+        comboTerm.addItem("Summer");
+
+        buildEnrollTable();
+        buildEnrollForm();
+        buildEnrollButtons();
+        buildEnlistTable();
+        buildEnlistForm();
+        buildEnlistButtons();
+
+        // Top table row click → load enlistments
+        enrollTable.getSelectionModel().addListSelectionListener(e ->{
+            if (!e.getValueIsAdjusting()){
+                int row = enrollTable.getSelectedRow();
+                if (row < 0) return;
+
+                selectedEnrollentId = (int) enrollModel.getValueAt(row, 0);
+
+                fieldEnrollId.setText(
+                    String.valueOf(selectedEnrollentId)
+                );
+
+                refreshEnlistTable();
+            }
+        });
+
+        // Bottom table row click → fill grade for update
+        enlistTable.getSelectionModel().addListSelectionListener(e->{
+            if (!e.getValueIsAdjusting()){
+                int row = enlisTable.getSelectedRow();
+                if (row < 0) return;
+
+                selectedEnlistmentId = (int) enlistModel.getValueAt(row, 0);
+
+                Object grade = enlistModel.getValueAt(row, 9);
+
+                fieldGrade.setText(grade != null ? grade.toString() : "");
+            }
+        });
+
+        refreshEnrollTable();
+    }
+
+    private void buildEnrollTable(){
+        enrollTable.setFont(myFont);
+        enrollTable.setRowHeight(24);
+
+        enrollTable.getTableHeader()
+            .setFont(new Font("Arial", Font.BOLD, 14));
+        enrollTable.getTableHeader()
+            .setBackground(Color.decode("#F5E642"));
+        enrollTable.getTableHeader()
+            .setForeground(Color.decode("#1a1a1a"));
+
+        enrollTable.setBackground(Color.decode("#2d2d1a"));
+        enrollTable.setGridColor(Color.decode("#555533"));
+        enrollTable.setForeground(Color.decode("#e0d060"));
+
+        enrollTable.setSelectionBackground(
+            Color.decode("#3a3a20"));
+        enrollTable.setSelectionForeground(
+            Color.decode("#F5E642"));
+
+        JScrollPane scroll = new JScrollPane(enrollTable);
+        scroll.setBounds(10, 10, 720, 140);
+        scroll.setBorder(BorderFactory.createLineBorder(
+            Color.decode("#F5E642")));
+
+        add(scroll);
+    }
+
+    private void buildEnrollForm() {
+
+        addLabel("Student:", 10, 163);
+        addLabel("Program:", 10, 198);
+        addLabel("School Year:", 10, 233);
+        addLabel("Term:", 10, 268);
+
+        //Style combo boxes
+        setFont(myFont);
+        setBackground(Color.decode("#2a2a2a"));
+        setForeground(Color.decode("#e0d060"));
+
+        comboStudent.setBounds(160, 160, 300, 28);
+        comboProgram.setBounds(160, 195, 280, 28);
+        
+        styleField(fieldSchoolYear);
+        fieldSchoolYear.setBounds(160, 230, 160, 28);
+
+        comboTerm.setBounds(160, 265, 160, 28);
+
+        //Add All 4
+        add(comboStudent);
+        add(comboProgram);
+        add(fieldSchoolYear);
+        add(comboTerm);
+    }
+
+    private void buildEnrollButtons() {
+
+        styleButton(btnEnroll, Color.decode("#F5E642"), Color.decode("#1a1a1a"));
+        styleButton(btnRefresh, Color.decode("#3a3a20"), Color.decode("#F5E642"));
+        styleButton(btnClearEnroll, Color.decode("#2a2a2a"), Color.decode("#888860"));
+
+        btnEnroll.setBounds(10, 305, 100, 35);
+        btnRefresh.setBounds(120, 305, 100, 35);
+        btnClearEnroll.setBounds(230, 305, 100, 35);
+
+        btnEnroll.addActionListener(e -> onEnroll());
+        btnRefresh.addActionListener(e -> refreshEnrollTable());
+        btnClearEnroll.addActionListener(e -> clearEnrollFields());
+
+        //Add All 3
+        add(btnEnroll);
+        add(btnRefresh);
+        add(btnClearEnroll);
+    }
+
+    private void onEnroll(){
+        
+        if (fieldSchoolYear.getText().isBlank()){
+            showError("School Year is required");
+            return;
+        }
+
+        try{
+            String combo = comboStudent.getSelectedItem().toString();
+
+            int studetId = Integer.parseInt(combo.split(" - ")[0].trim());
+
+            String program = comboProgram.getSelectedItem().toString();
+            String sy = fieldSchoolYear.getText().trim();
+            String term = comboTerm.getSelectedItem().toString();
+
+            dbManager.enrollStudent(studentId, program, sy, term);
+
+            refreshEnrollTable();
+            clearEnrollFields();
+
+        } catch (NumberFormatException e){
+            showError("Invalid student selection.");
+        } catch (SQLException ex){
+            showError("Failed to enroll: " + ex.getMessage());
+        }
+    }
+
+    private void refreshEnrollTable(){
+        enrollModel.setRowCount(0);
+
+        try{
+            for (Object[] r : dbManager.getAllEnrollments()){
+                enrollModel.addRow(r);
+            }
+        } catch (SQLException ex){
+            showError("Failed to load enrollments.");
+        }
+    }
+
+    private void clearEnrollFields(){
+        
+        comboStudent.setSelectedIndex(0);
+        comboProgram.setSelectedIndex(0);
+        fieldSchoolYear.setText("");
+        comboTerm.setSelectedIndex(0);
+
+        enrollTable.clearSelection();
+        selectedEnrollmentId = -1;
+
+        enlistModel.setRowCount(0);
+        fieldEnrollId.setText("");
     }
 }
